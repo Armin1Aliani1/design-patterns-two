d5d107d22eb9f6f6b7ae74304b6ea490ed4979dc
Dragon2623 <armin1.aliani1@gmail.com>
2022-07-08 10:53:48 +0430

    add interface HouseBuilder and HousePlan

---
+++ designPatternsTwo/designPatternsTwo/src/main/java/com/designPatterns/creationalPatterns/builder/HouseBuilder.java
@@ -0,0 +1,10 @@
+package com.designPatterns.creationalPatterns.builder;
+
+public interface HouseBuilder {
+    void buildBasement();
+    void buildStructure();
+    void buildRoof();
+    void buildInterior();
+
+    House getHouse();
+}
---
+++ designPatternsTwo/designPatternsTwo/src/main/java/com/designPatterns/creationalPatterns/builder/HousePlan.java
@@ -0,0 +1,8 @@
+package com.designPatterns.creationalPatterns.builder;
+
+public interface HousePlan {
+    void setBasement(String basement);
+    void setStructure(String structure);
+    void setRoof(String roof);
+    void setInterior(String interior);
+}
