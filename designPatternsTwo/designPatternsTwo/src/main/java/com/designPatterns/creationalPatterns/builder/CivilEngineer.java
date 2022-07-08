78d004a52cfc50076a3e9bfa7a1952d31b97e630
Dragon2623 <armin1.aliani1@gmail.com>
2022-07-08 10:55:55 +0430

    add House and CivilEngineer

---
+++ designPatternsTwo/designPatternsTwo/src/main/java/com/designPatterns/creationalPatterns/builder/CivilEngineer.java
@@ -0,0 +1,20 @@
+package com.designPatterns.creationalPatterns.builder;
+
+public class CivilEngineer {
+    private HouseBuilder houseBuilder;
+
+    public CivilEngineer(HouseBuilder houseBuilder) {
+        this.houseBuilder = houseBuilder;
+    }
+
+    public void constructHouse() {
+        this.houseBuilder.buildBasement();
+        this.houseBuilder.buildStructure();
+        this.houseBuilder.buildRoof();
+        this.houseBuilder.buildInterior();
+    }
+
+    public House getHouse() {
+        return this.houseBuilder.getHouse();
+    }
+}
